d75e772ff2a9d87b0afb84281d14b187de723666
MANAVmandalia78 <manavmandalia077@gmail.com>
2026-01-01 23:29:10 +0530

    A

---
+++ to-do-App/src/Components/Addtodo.jsx
@@ -0,0 +1,17 @@
+function Addtodo(){
+    return <div class="row kg-row">
+            <div class="col-6">
+              <input type="text" placeholder="Enter Todo Here" />
+            </div>
+            <div class="col-4">
+              <input type="date" />
+            </div>
+            <div class="col-2">
+              <button type="button" class="btn btn-success">
+                Success
+              </button>
+            </div>
+          </div>
+}
+
+export default Addtodo;
---
+++ to-do-App/src/Components/Item2.jsx
@@ -0,0 +1,21 @@
+function Item2(){
+    let itemName = "Go to Collage"
+    let date = " 4/10/2026"
+return <div class="container text-center">
+          <div class="row kg-row">
+            <div class="col-6">
+              {itemName}
+            </div>
+            <div class="col-4">
+              {date}
+            </div>
+            <div class="col-2">
+              <button type="button" class="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+}
+
+export default Item2;
