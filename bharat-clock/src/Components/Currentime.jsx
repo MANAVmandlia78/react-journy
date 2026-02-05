af47b776842f1f543b9c4aae77b2534b5f50f743
MANAVmandalia78 <manavmandalia077@gmail.com>
2026-02-06 01:15:15 +0530

    A

--- bharat-clock/src/Components/Currentime.jsx
+++ bharat-clock/src/Components/Currentime.jsx
@@ -1,9 +1,24 @@
-function Currentime(){
-    let time = new Date();
-return <p className="lead">
-    This is the Currentime: {time.toLocaleDateString()} -{" "}
-    {time.toLocaleTimeString()}.
+import { useEffect, useState } from "react";
+
+function Currentime() {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    console.log("Interval has been setup")
+    const IntervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+        clearInterval(IntervalId);
+        console.log("Cancle The Interval")
+    }
+  }, []);
+  return (
+    <p className="lead">
+      This is the Currentime: {time.toLocaleDateString()} -{" "}
+      {time.toLocaleTimeString()}.
     </p>
+  );
 }
 
 export default Currentime;
